37acca4e50e02cb2ac711f8a140dae0d3dbcf64a
ukomiljon <ukomiljon@users.noreply.github.com>
2020-08-12 15:18:39 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,1 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import { currency } from "./currency"
-
-Vue.config.productionTip = false;
-Vue.filter("currency", currency)
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+import "./PresentationLayer/main"
